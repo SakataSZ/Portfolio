3a3b5c8179b63ed3bf6c593246dcfef54587ef55
SakataSZ <sakamoto.yuta.sz@gmail.com>
2022-07-23 03:42:33 +0900

    update routing

--- src/App.jsx
+++ src/App.jsx
@@ -5,20 +5,20 @@ import About from './pages/About';
 import Contact from './pages/Contact';
 import ResponsiveDrawer from './components/ResponsiveDrower';
 
-// const baseURL = process.env.PUBLIC_URL
+const ROUTER_BASENAME = process.env.NODE_ENV === 'development' ? '/' : '/repo-name';
 const baseURL = '';
 
 const pages = [
-  {name: 'Home', path: baseURL + '/', element: <Home />},
-  {name: 'About', path: baseURL + '/about', element: <About />},
-  {name: 'Contact', path: baseURL + '/contact/', element: <Contact />}
+  { name: 'Home', path: baseURL + '/', element: <Home /> },
+  { name: 'About', path: baseURL + '/about', element: <About /> },
+  { name: 'Contact', path: baseURL + '/contact/', element: <Contact /> }
 ]
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <ResponsiveDrawer pages={pages}/>
+      <BrowserRouter basename={ROUTER_BASENAME}>
+        <ResponsiveDrawer pages={pages} />
         <Routes>
           {pages.map((page) => (
             <Route path={page.path} element={page.element} key={page.name} />
